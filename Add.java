584a21bbba938d922ac37ab0ae8bd1f4def725f2
susmithalks1594 <susmithalks1594@gmail.com>
2024-07-11 16:32:55 +0530

    commit-msg

---
+++ Add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Add
+{
+public static void main(String args[])
+{
+int a=90;
+int b=80;
+int c= a+b;
+System.out.println("c="+c);
+}
+}
---
+++ Operations.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class  OPerations
+{
+public static void main(String args[])
+{
+int c=90,d=80,e=7;
+int f=c*d;
+int g=d-f;
+System.out.println("f="+f+" "+g);
+}
+}
